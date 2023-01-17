28797bff16d73837f32fd60a20966f331e8e1323
jrh <joshuaharris1103@gmail.com>
2023-01-17 11:52:38 -0500

    add fruit model and seed routes

---
+++ server.js
@@ -0,0 +1,46 @@
+///////////////////////////////////////
+//// Import Dependencies           ////
+///////////////////////////////////////
+const express = require('express') //import the express framework
+const mongoose = require('mongoose') //import the mongoose framework
+const morgan = require('morgan') //import the morgan request logger
+require('dotenv').config() // Load my ENV file's Variables
+
+///////////////////////////////////////
+//// Database Connection           ////
+///////////////////////////////////////
+// this is where we will set up our inputs for our database connect function
+const DATABASE_URL = process.env.DATABASE_URL
+// here is our DB config object
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// establish our db connection
+mongoose.connect(DATABASE_URL, CONFIG)
+
+// Tell mongoose what to do with certain events
+// what happens when we open, disconnect, or get and error
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', (err) => console.log('An error occurred: \n', err))
+
+///////////////////////////////////////
+//// Create our Express App Object ////
+///////////////////////////////////////
+const app = express()
+
+
+
+
+
+
+///////////////////////////////////////
+//// Server Listener               ////
+///////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
+
+//END OF FILE
